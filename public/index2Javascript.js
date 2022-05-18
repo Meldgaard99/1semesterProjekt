9055a079592a2f48facf23e30349f0b722355c6a
boes <boes9@icloud.com>
2022-05-18 16:58:17 -0400

    nu fuldt fungerende

--- public/index2Javascript.js
+++ public/index2Javascript.js
@@ -1,13 +1,6 @@
 
-console.log("Say something")
-d3.select("#test")
-    .on("click", function () {
-        console.log("Hi my friend")
-    })
 
-
-
-d3.select("#januar")
+d3.select("#Januar")
     .on("click", function () {
         console.log("Januar button clicked")
         d3.json(`/api/frugt/getco2indud`, {
@@ -16,23 +9,32 @@ d3.select("#januar")
 
             const data = response.data; // Hent data ud af response
             seasonal = data
-            console.log(`Data from "seasonalVegatabels1": ${response.data[0].Groentsag}`);
+            console.log(`Data from "seasonalVegatabels1": ${response.data[0].grøntsag}`);
 
             for (let index1 = 0; index1 < seasonal.length; index1++) {
                 const style1 = `border:1px solid black`;
-                const tmpName = seasonal[index1].Groentsag;
-                console.log(tmpName)
+                const tmpName = seasonal[index1].grøntsag;
                 const button = d3.select(".kalender")
                     .append("button")
                     .attr("style", style1)
-                    .attr("id", tmpName)
+                    .attr("id", `${tmpName}1`)
                     .attr("class", "fruit-card-btn")
                     .attr("background-image", `url("/grøntsager-realistisk/${tmpName}.png")`)
                     .on("click", function () {
                         console.log(`${tmpName} clicked`)
                     });
 
+                // Koden der indsætter billede til den tilsvarende grøntsag - når der køres loop.
+                const image1 = document.createElement("img");
+                image1.src = `grøntsager-realistisk/${tmpName}.png`;
+                image1.width = 50
+                image1.height = 50
+                document.getElementById(`${tmpName}1`).appendChild(image1);
             }
+
+
+
+
         })
 
 
@@ -70,11 +72,3 @@ for (let index1 = 0; index1 < seasonal.length; index1++) {
 })
 */
 
-/*
-// Koden der indsætter billede til den tilsvarende grøntsag - når der køres loop.
-const image1 = document.createElement("img");
-image1.src = `grøntsager-realistisk/${tmpName}.png`;
-image1.width = 50
-image1.height = 50
-document.getElementById(`${tmpName}`).appendChild(image1);
-*/
